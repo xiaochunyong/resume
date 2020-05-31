16024a4a82d60008ec67c53763ad821bf6e6a3f7
Ely.Xiao <xiaochunyong@gmail.com>
2020-05-31 15:17:29 +0800

    update

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
-git pull
-mkdir -p /usr/share/nginx/resume/static
-cp index.html /usr/share/nginx/resume
-cp resume.pdf /usr/share/nginx/resume
-cp static/app.js /usr/share/nginx/resume/static/app.js
+npm run build
+scp index.html root@103.205.9.16:/usr/share/nginx/resume/
+scp resume.pdf root@103.205.9.16:/usr/share/nginx/resume/
+scp static/app.js root@103.205.9.16:/usr/share/nginx/resume/static
+# git pull
+# mkdir -p /usr/share/nginx/resume/static
+# cp index.html /usr/share/nginx/resume
+# cp resume.pdf /usr/share/nginx/resume
+# cp static/app.js /usr/share/nginx/resume/static/app.js
 echo "部署完成"
